4f8f9ea55ce83f2afd515bd108957728124d79fc
shinkarom <32741748+shinkarom@users.noreply.github.com>
2017-10-26 12:44:08 +0300

    Added FifteenGame class.

---
+++ Fifteen/FifteenGame.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "FifteenGame.h"
+
+
+auto FifteenGame::GenerateNew() -> void
+{
+}
+
+FifteenGame::FifteenGame()
+{
+}
+
+
+FifteenGame::~FifteenGame()
+{
+}
---
+++ Fifteen/FifteenGame.h
@@ -0,0 +1,10 @@
+#pragma once
+class FifteenGame
+{
+public:
+	auto GenerateNew() ->void;
+	FifteenGame();
+	~FifteenGame();
+
+};
+
