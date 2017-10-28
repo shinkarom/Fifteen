3a749bf9cbe4a6ff841b14b3fcbddf51d2dbed8d
shinkarom <32741748+shinkarom@users.noreply.github.com>
2017-10-28 17:03:09 +0300

    Implemented the full game.

--- Fifteen/FifteenGame.cpp
+++ Fifteen/FifteenGame.cpp
@@ -1,8 +1,49 @@
 #include "stdafx.h"
 #include "FifteenGame.h"
 #include <randutils.hpp>
+#include <utility>
+#include <algorithm>
 
 using namespace randutils;
+
+int FifteenGame::GetIndex(int num)
+{
+	auto d = find(Playfield.begin(), Playfield.end(), num);
+	if (d == Playfield.end()) throw exception("Wrong coordinate");
+	return d - Playfield.begin();
+}
+
+pair<int, int> FifteenGame::GetCoord(int num)
+{
+
+	int index = GetIndex(num);
+	return make_pair(index % 4, index / 4);
+}
+
+bool FifteenGame::IsALegalMove(int num)
+{
+	if ((num < 0) || (num > 15)) return false;
+	auto spot = GetCoord(num);
+	auto hole = GetCoord(16);
+	bool left = ((hole.first > 0) && (spot.first == hole.first - 1));
+	bool right = ((hole.first < 4 - 1) && (spot.first == hole.first + 1));
+	bool top = ((hole.second > 0) && (spot.second == hole.second - 1));
+	bool bottom = ((hole.second < 4 - 1) && (spot.second == hole.second + 1));
+	return (left || right) ^ (top || bottom);
+}
+
+bool FifteenGame::IsBoardWon()
+{
+	for (int x = 0;x < 16 + 1;x++) { if (Playfield[x] != x + 1) return false; }
+	return true;
+}
+
+void FifteenGame::MakeTheMove(int num)
+{
+	auto spot = GetIndex(num);
+	auto hole = GetIndex(16);
+	swap(Playfield[spot], Playfield[hole]);
+}
 
 int FifteenGame::GetCell(int x, int y)
 {
@@ -12,7 +53,7 @@ int FifteenGame::GetCell(int x, int y)
 void FifteenGame::GenerateNew()
 {
 	mt19937_rng gen;
-	gen.shuffle(Playfield);
+	gen.shuffle(Playfield.begin(), Playfield.begin() + 15);
 }
 
 FifteenGame::FifteenGame()
--- Fifteen/FifteenGame.h
+++ Fifteen/FifteenGame.h
@@ -1,17 +1,22 @@
 #pragma once
 #include "stdafx.h"
 #include <vector>
-
+#include <utility>
 using namespace std;
 
 class FifteenGame
 {
-private:
-	vector<int> Playfield;
 public:
+	bool IsALegalMove(int num);
+	bool IsBoardWon();
+	void MakeTheMove(int num);
 	int GetCell(int x, int y);
 	void GenerateNew();
 	FifteenGame();
 	~FifteenGame();
+private:
+	vector<int> Playfield;
+	int GetIndex(int num);
+	pair<int,int> GetCoord(int num);
 };
 
