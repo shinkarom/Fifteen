a8db35408ef22780223386bf7890e92bad7f391a
shinkarom <32741748+shinkarom@users.noreply.github.com>
2017-10-28 11:33:56 +0300

    Implemented DrawGame

--- Fifteen/FifteenGame.cpp
+++ Fifteen/FifteenGame.cpp
@@ -1,15 +1,24 @@
 #include "stdafx.h"
 #include "FifteenGame.h"
+#include <randutils.hpp>
 
+using namespace randutils;
+
+int FifteenGame::GetCell(int x, int y)
+{
+	return Playfield.at(y * 4 + x);
+}
 
 void FifteenGame::GenerateNew()
 {
-	Playfield.clear();
-	for (int x = 1;x <= 16;x++) Playfield.push_back(x);
+	mt19937_rng gen;
+	gen.shuffle(Playfield);
 }
 
 FifteenGame::FifteenGame()
 {
+	Playfield.clear();
+	for (int x = 1;x <= 16;x++) Playfield.push_back(x + 1);
 	GenerateNew();
 }
 
--- Fifteen/FifteenGame.h
+++ Fifteen/FifteenGame.h
@@ -9,6 +9,7 @@ class FifteenGame
 private:
 	vector<int> Playfield;
 public:
+	int GetCell(int x, int y);
 	void GenerateNew();
 	FifteenGame();
 	~FifteenGame();
